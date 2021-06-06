022ca8b50e56070818585424f90d76be6d7c71bb
Patrick Chow <patrickchow0803@gmail.com>
2021-06-06 09:38:01 -0500

    generator now works properly

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -13,12 +15,35 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  // nullable since the number isn't generated yet
+  int? _generatedNumber;
+
+  final randomGenerator = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Randomizer Generator'),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? 'Generate a Number',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
