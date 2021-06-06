0985fbecf300f14c08c566a2ef36a5333f06872b
Patrick Chow <patrickchow0803@gmail.com>
2021-06-06 11:34:49 -0500

    App now works with provider

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,39 +1,38 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_foundation/randomizer_change_notifier.dart';
+import 'package:provider/provider.dart';
 
-class RandomizerPage extends HookWidget {
-  final int min, max;
-
-  final randomGenerator = Random();
-
+class RandomizerPage extends StatelessWidget {
   RandomizerPage({
     Key? key,
-    required this.min,
-    required this.max,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final generatedNumber = useState<int?>(null);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Randomizer Generator'),
       ),
       body: Center(
-        child: Text(
-          generatedNumber.value?.toString() ?? 'Generate a Number',
-          style: TextStyle(
-            fontSize: 40,
-          ),
+        // can't use context.read here because you want the UI to update properly
+        // therefore use the consumer widget so that the changeNotifier is called in the randomizer_change_notifier file
+        child: Consumer<RandomizerChangeNotifier>(
+          builder: (context, notifier, child) {
+            return Text(
+              notifier.generatedNumber?.toString() ?? 'Generate a Number',
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text('Generate'),
         onPressed: () {
-          generatedNumber.value = min + randomGenerator.nextInt(max + 1 - min);
+          context.read<RandomizerChangeNotifier>().generateRandomNumber();
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
